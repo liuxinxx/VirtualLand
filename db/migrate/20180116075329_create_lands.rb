4b8860540dde30e62837caf5526d89a107002780
myvary <727484328@qq.com>
2018-01-16 15:53:52 +0800

    修改模型

--- db/migrate/20180116075329_create_lands.rb
+++ db/migrate/20180116075329_create_lands.rb
@@ -1,7 +1,7 @@
 class CreateLands < ActiveRecord::Migration[5.1]
   def change
     create_table :lands do |t|
-      t.initeger :user_id
+      t.integer :user_id
       t.text :latlng
 
       t.timestamps
