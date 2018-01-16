a3cb86da1398ba364ebe3f070a82ad36ecb20881
dccmmtop <dccmmtop@gmail.com>
2018-01-16 20:05:14 +0800

    修改代码格式

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,12 @@
 module ApplicationHelper
   def notice_message
     flash_messages = []
-
     flash.each do |type, message|
       type = :success if type.to_sym == :notice
       type = :danger  if type.to_sym == :alert
       text = content_tag(:div, link_to(raw('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'), '#', :class => 'close', 'data-dismiss' => 'alert') + message, class: "alert alert-#{type}")
       flash_messages << text if message
     end
-
     flash_messages.join("\n").html_safe
   end
 end
